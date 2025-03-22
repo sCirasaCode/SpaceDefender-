10508169833508132a0920e350383bd695200a15
sCirasaCode <cirasa.silvano@gmx.ch>
2025-03-22 19:06:51 +0100

    init commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <raylib-cpp.hpp>
+#include "raylib-cpp.hpp"
 
 int main() {
     
@@ -7,21 +7,30 @@ int main() {
     int screenHeight = 450;
 
     raylib::Color textColor(LIGHTGRAY);
-    raylib::Window w(screenWidth, screenHeight, "Raylib C++ Starter Kit Example");
+    raylib::Window window(screenWidth, screenHeight, "Raylib C++ Starter Kit Example");
+
+    raylib::Vector2 playerPosition(screenWidth / 2, screenHeight / 2);
+    
+    float playerSpeed = 2.0f;
+
     
     SetTargetFPS(60);
 
     // Main game loop
-    while (!w.ShouldClose()) // Detect window close button or ESC key
+    while (!window.ShouldClose()) // Detect window close button or ESC key
     {
         // Update
 
-        // TODO: Update your variables here
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            playerPosition = GetMousePosition();
+        }
 
         // Draw
         BeginDrawing();
         ClearBackground(RAYWHITE);
         textColor.DrawText("Congrats! You created your first window!", 190, 200, 20);
+        DrawCircleV(playerPosition, 20, MAROON);
         EndDrawing();
     }
 
